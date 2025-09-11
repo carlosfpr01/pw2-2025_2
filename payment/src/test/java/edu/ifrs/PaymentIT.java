78b7f96788019eaf08d57dab627aa77ce135da28
Carlos Felipe Ramos Pires <141787491+carlosfpr01@users.noreply.github.com>
2025-09-11 23:35:20 +0000

    ajusteProjetos

--- payment/src/test/java/edu/ifrs/PaymentIT.java
+++
@@ -1,8 +0,0 @@
-package edu.ifrs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class PaymentIT extends PaymentTest {
-    // Execute the same tests but in packaged mode.
-}
--- users/src/main/java/dev/rpmhub/web/Users.java
+++ users/src/main/java/dev/rpmhub/web/Users.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import org.eclipse.microprofile.jwt.Claims;
 
 import io.smallrye.jwt.build.Jwt;
+import io.vertx.core.json.JsonObject;
 import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -43,7 +44,12 @@ public class Users {
     @PermitAll
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public String generate(final String fullName, final String email) {
+    public String generate(final String json) {
+
+        JsonObject  jsonObject = new JsonObject(json);
+        String email = jsonObject.getString("email");
+        String fullName = jsonObject.getString("fullName");
+
         return Jwt.issuer(ISSUER)
                 .upn(email)
                 .groups(new HashSet<>(Arrays.asList("User", "Admin")))
